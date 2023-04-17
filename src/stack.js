29407127ee5f3065d4e9f256dcc137a68304bbc6
kate-rzk <razkovaekaterina@gmail.com>
2023-04-18 01:57:19 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(item);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items == null) return null;
+    return this.items.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items == null) return null;
+    return this.items[this.items.length - 1];
   }
 }
 
